76ddd51e13e995659236d500dff8caf24f6e1f94
Yessica Turitich <yessi_turitich@hotmail.com>
2022-10-17 22:51:01 -0300

    pre-entrega2

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
-
-//let usuario = prompt("Ingresar usuario");
-//let clave = prompt("Ingresar clave");
-
-//if(usuario == "yessituri" && clave == "riverplate"){
-   // alert(`Bienvenida ${usuario}`);
-//} else {
- //   alert("Acceso denegado");
-//}
+/**
+let usuario = prompt("Ingresar usuario");
+let clave = prompt("Ingresar clave");
 
 
-//function appTarea () {
-   // for (let i=0 ; i<3 ; i++){
-       // let nuevaTarea = prompt("esta es una app de tareas, quieres definir una nueva tarea? \n Ingrese si o no");
-    //if(nuevaTarea == "si") {
-       // let tarea = prompt("Ok, definamos la tarea: que tienes que hacer?");
-        //alert("Excelente, acuerdate de " + tarea);
-        //console.log("tarea ingresada:" + tarea);
-   // } //else if (nuevaTarea == "no") {
-       // alert("Ok, no hay nada por hacer");
-     //   break
-   // } else {
-     //   alert("No ingreso una respuesta valida, vuelva a intentar");
-    //}
-    //}
-//}
+    if(usuario == "yessituri" && clave == "riverplate"){
+    alert(`Bienvenida ${usuario}`);} 
+else {alert("Acceso denegado");
+}
 
-//appTarea();
-
-// Funcion para calcular las notas
+function appTarea () {
+    for (let i=0 ; i<3 ; i++){
+        let nuevaTarea = prompt("esta es una app de tareas, quieres definir una nueva tarea? \n Ingrese si o no");
+    if(nuevaTarea == "si") {
+        let tarea = prompt("Ok, definamos la tarea: que tienes que hacer?");
+        alert("Excelente, acuerdate de " + tarea);
+        console.log("tarea ingresada:" + tarea);
+    } else if (nuevaTarea == "no") {
+    alert("Ok, no hay nada por hacer");
+    break
+    } else {
+    alert("No ingreso una respuesta valida, vuelva a intentar");
+    }
+    }
+}
+appTarea();
+ Funcion para calcular las notas/** */
 const calcular = () => {
 
     // input del estudiante para cada variable
@@ -36,7 +33,7 @@ const calcular = () => {
     let matematica = document.querySelector("#matematica").value;
     let lengua = document.querySelector("#lengua").value;
     let notas = "";
-    
+
     let totalNotas =
         parseFloat(fisica) +
         parseFloat(ingles) +
@@ -54,38 +51,36 @@ const calcular = () => {
     } else {
         notas = "Desaprobado";
     }
-  
+
     // Checkeo por datos vacios
-    if (fisica == "" || ingles == ""
-                || matematica == "" || lengua == "") {
-        document.querySelector("#mostrarDatos").innerHTML
-            = "Ingrese datos correctos";
+    if (fisica == "" || ingles == "" ||
+        matematica == "" || lengua == "") {
+        document.querySelector("#mostrarDatos").innerHTML = "Ingrese datos correctos";
     } else {
-    
+
         // Checkear si la condicion pasa o falla
         if (porcentaje >= 39.5) {
-        document.querySelector(
-            "#mostrarDatos"
-        ).innerHTML =
-            ` De un total de 40 es ${totalNotas}
+            document.querySelector(
+                    "#mostrarDatos"
+                ).innerHTML =
+                ` De un total de 40 es ${totalNotas}
             y el porcentaje es ${porcentaje}%. <br>
             Tu nota es ${notas}. Aprobaste. `;
         } else {
-        document.querySelector(
-            "#mostrarDatos"
-        ).innerHTML =
-            ` De un total de 40 es ${totalNotas}
+            document.querySelector(
+                    "#mostrarDatos"
+                ).innerHTML =
+                ` De un total de 40 es ${totalNotas}
             y el porcentaje es  ${porcentaje}%. <br>
             Tu nota es ${notas}. Desaprobaste. `;
         }
     }
-    };
-    
-    document.getElementById('captcha')
-    document.addEventListener('keyup', function(event) {
-        if (event.code === 'Enter')
-        {
-            event.preventDefault();
+};
 
-        }
-    });
+document.getElementById('captcha')
+document.addEventListener('keyup', function (event) {
+    if (event.code === 'Enter') {
+        event.preventDefault();
+
+    }
+});
