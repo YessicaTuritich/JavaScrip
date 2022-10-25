bea47b9244fb0f0d8a2d9d975c2a1c9656160775
Yessica Turitich <yessi_turitich@hotmail.com>
2022-10-25 11:58:27 -0300

    Re-entrega2

--- array.js
+++ array.js
@@ -1,89 +1,132 @@
-//Declaracion de variables
-let estudiante;
+//Variables
+let usuarioEstudiante;
 let password;
+let salir = false;
+let opcion;
+
+//Declaracion de constructor
+class UsuarioEstudiante {
+    constructor(user, password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    saludo() {
+        alert("Bienvenido de nuevo:" + this.user);
+    }
+}
 
 
-//Declaracion de array
-const estudiantes = [];
 
+//Array
+const Usuarios = [];
 
-function validarString(){
-    if(!estudiante.match("[A-Za-z]{3,10}")){
-        do{
-            alert("Ingrese un nombre y apellidos validos");
-            estudiante = prompt("Ingrese nombre y apellido, solo caracteres alfabeticos:");
-        }while(!estudiante.match("[A-Za-z]{3,10}"));
+//Validar usuario
+function validarString() {
+    if (!usuarioEstudiante.match("[A-Za-z]{3,10}")) {
+        do {
+            alert("Ingrese un usuario valido");
+            usuarioEstudiante = prompt("Ingrese su nombre y apellido (Minimo 3 caracteres): ");
+        } while (!usuarioEstudiante.match("[A-Za-z]{3,10}"));
         return true;
     } else {
         return true;
     }
 }
 
-function validarestudiante(estudiante){
-    if (estudiantes.some(estudianteA => estudianteA.estudianteB === estudiante)){
+//Valida al usuario dentro del array
+function validarUser(usuarioEstudiante) {
+    if (Usuarios.some(username => username.user === usuarioEstudiante)) {
         return true;
     } else {
         return false;
     }
 }
 
-//pw de 8 caracteres o mas
-function validarPassword(){
-    if(password.length < 9){
-        do{
-            alert("Contraseña invalida");
-            password = prompt("Ingrese una contraseña de 9 caracteres o más: ");
-        }while(password.length < 9);
+function validarPassword() {
+    if (password.length < 9) {
+        do {
+            alert("Ingrese una contraseña valida");
+            password = prompt("Ingres de nuevo una contraseña de 9 caracteres como minimo: ");
+        } while (password.length < 9);
         return true;
     } else {
         return true;
     }
 }
 
-//Comparar y ordenar por orden alfabetico
-function compare( a, b ) {
-    if ( a.estudianteB < b.estudianteB){
+
+function compare(a, b) {
+    if (a.user < b.user) {
         return -1;
     }
-    if ( a.estudianteB > b.estudianteB){
+    if (a.user > b.user) {
         return 1;
     }
     return 0;
-}  
+}
 
-do{
+
+do {
     //Menu alert
-    opciones = prompt("Para registro de estudiante ingrese la letra: 'f'");
-    switch(opciones){
-
-
-        case 'f':
-            estudiante = prompt("ingrese su nombre (Minimo 3 caracteres): ");
+    opcion = prompt("Bienvenido de nuevo alumno por favor ingrese una opcion para continuar: \n i: Para iniciar sesion \n r: Para crear una cuenta \n s: para cerrar el menu");
+    switch (opcion) {
+        case 'i':
+            usuarioEstudiante = prompt("ingrese su usuario: ");
             validarString();
-            password = prompt("ingrese su contraseña (8 caracteres o más): ");
+            validarUser(usuarioEstudiante);
+            password = prompt("ingrese su contraseña: ");
             validarPassword();
 
-            //Validacion para estudiante ya ingresado
-            if(validarString() && validarPassword()){
-                if(!validarestudiante(estudiante)) {
-                    estudiante.push(new estudiante (estudiante,password));
-                    alert("Alumno asignado correctamente");
-                } else {
-                    do{
-                        alert("Alumno ya registrado");
-                        estudiante = prompt("Ingrese su nombre: ");
-                        validarString();
-                    }while(validarestudiante(estudiante));
+            if (validarUser(usuarioEstudiante) && validarPassword()) {
 
-                    estudiantes.push(new estudiante(estudiante,password));
-                    alert("Alumno asignado correctamente");
-                    
+                for (const _Usuario of UsuarioEstudiante) {
+                    if (usuarioEstudiante === UsuarioEstudiante.user) {
+                        _Usuario.saludo();
+                        break;
+                    }
+                }
+            } else {
+                alert("El estudiante no esta registrado");
+            }
+
+            break;
+
+        case 'r':
+            usuarioEstudiante = prompt("Ingrese su usuario (Nombre y Apellido): ");
+            validarString();
+            password = prompt("Ingrese su usuario (Nombre y Apellido): ");
+            validarPassword();
+
+            //Se valida que el estudiante que quiera registarse este disponible
+            if (validarString() && validarPassword()) {
+                if (!validarUser(usuarioEstudiante)) {
+                    Usuarios.push(new UsuarioEstudiante(usuarioEstudiante, password));
+                    alert("Estudiante correctamente registrado");
+                } else {
+                    do {
+                        alert("Estudiante ya registrado");
+                        usuarioEstudiante = prompt("ingrese su usuario: ");
+                        validarString();
+                    } while (validarUser(usuarioEstudiante));
+
+                    Usuarios.push(new UsuarioEstudiante(usuarioEstudiante, password));
+                    alert("Alumno registrado correctamente");
+
                 }
 
             }
+
             break;
 
+
+
+        case 's':
+            salir = true;
+            alert("Suerte y a estudiar!")
+
+            break;
         default:
-            alert("ingrese una opcion valida!");
+            alert("Opcion invalida, pruebe de nuevo");
     }
-}while(salir == false);
+} while (salir == false);
